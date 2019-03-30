ca79cb4995f1f09fb8cc85a16dbf81bc159577af
Roman Mykhailiuk <mikhailiuk.roman@gmail.com>
2019-03-30 10:40:38 +0200

    1

--- js/piramid.js
+++ js/piramid.js
@@ -1,15 +1,15 @@
 
-function printPyramid(n) {  //Input or number of rows
-for (var i = 1; i <= n; i++) {
+function printPyramid(n) {  
+for (let i = 1; i <= n; i++) {
     let pyramid_row = "";
-    for (let j = 1; j <= (2 * n - 1); j++) {   ////For every each counter there exist 2*n-1 value
+    for (let j = 1; j <= (2 * n - 1); j++) {   
        if (j >= n + 1 - i && j <= n - 1 + i) {
        pyramid_row += "*"
        } else{
        pyramid_row += " ";
-       }   //Check the work book image
+       }  
     }
    return pyramid_row;
 }
 }
-printPyramid(5)   //Input or number of rows
+printPyramid(5)  
